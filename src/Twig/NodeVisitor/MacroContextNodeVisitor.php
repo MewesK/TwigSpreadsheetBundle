5a34d68126b41a256ef24c51747843cbb5f19ae0
Mewes Kochheim <mewes@kochheim.de>
2017-07-02 17:06:43 +0200

    Bug fix

--- src/Twig/NodeVisitor/MacroContextNodeVisitor.php
+++ src/Twig/NodeVisitor/MacroContextNodeVisitor.php
@@ -22,16 +22,19 @@ class MacroContextNodeVisitor extends \Twig_BaseNodeVisitor
      */
     protected function doEnterNode(\Twig_Node $node, \Twig_Environment $env)
     {
-        // Add main spreadsheet wrapper as argument on method/macro calls
+        // add wrapper instance as argument on all method calls
         if ($node instanceof \Twig_Node_Expression_MethodCall) {
+            $keyNode = new \Twig_Node_Expression_Constant(PhpSpreadsheetWrapper::INSTANCE_KEY, $node->getTemplateLine());
+
+            // add wrapper even if it not exists, we fix that later
+            $valueNode = new \Twig_Node_Expression_Name(PhpSpreadsheetWrapper::INSTANCE_KEY, $node->getTemplateLine());
+            $valueNode->setAttribute('ignore_strict_check', true);
+
             /**
              * @var \Twig_Node_Expression_Array $argumentsNode
              */
             $argumentsNode = $node->getNode('arguments');
-            $argumentsNode->addElement(
-                new \Twig_Node_Expression_Name(PhpSpreadsheetWrapper::INSTANCE_KEY, $node->getTemplateLine()),
-                new \Twig_Node_Expression_Constant(PhpSpreadsheetWrapper::INSTANCE_KEY, $node->getTemplateLine())
-            );
+            $argumentsNode->addElement($valueNode, $keyNode);
         }
 
         return $node;
