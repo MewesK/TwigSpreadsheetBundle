0b8b617f27d40d595d04046754ad5b383001f100
Mewes Kochheim <mewes@kochheim.de>
2018-06-24 22:18:24 +0200

    Fix: Removed badges from the RTD docs Updated copyright year

--- src/Resources/doc/conf.py
+++ src/Resources/doc/conf.py
@@ -2,7 +2,7 @@
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'TwigSpreadsheetBundle'
-copyright = u'2017, Mewes Kochheim'
+copyright = u'2018, Mewes Kochheim'
 version = ''
 release = ''
 html_theme = 'default'
